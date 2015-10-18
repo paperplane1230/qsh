3e019371287541d27f304f3bf6fee3a1cce91071
qyl <jsyz_qyl@163.com>
2015-10-18 10:54:09 +0800

    Build the environment.

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Author: Alan Chien
+ * Email: upplane1230@gmail.com
+ * Language: C
+ * Date: Sun Oct 18 09:20:42 CST 2015
+ * Description: The "main" module of qsh.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    fputs("qsh\n", stdout);
+    return 0;
+}
+
